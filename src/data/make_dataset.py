77bf2814c3b75d290b81dec9f8f3db28af539a78
Stanzin Angmo <stanzinangmo32925@gmail.com>
2023-12-05 19:49:02 +0530

    first commit

---
+++ src/data/make_dataset.py
@@ -0,0 +1,4 @@
+import pandas as pd
+# 1. Reading data from CSV
+def read_csv(file_path):
+    return pd.read_csv(file_path)
---
+++ src/features/build_features.py
@@ -0,0 +1,4 @@
+# 2. Creating features
+def create_features(data):
+    # No feature creation for this example
+    return data
